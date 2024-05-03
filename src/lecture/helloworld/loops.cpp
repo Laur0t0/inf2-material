d3a83a319cd2422af1fabee3a2904ae0e1567f66
Laur0t0 <corvin2505@icloud.com>
2024-05-03 10:51:23 +0200

    03.05.

---
+++ src/lecture/helloworld/loops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    int i = 38;
+
+    for (int i = 1; i <= 10; i++){
+
+        std::cout << i << " "; //mit leerzeichen zwischen den zahlen
+        //std::cout << i << "\n"; //mit leerer zeile zwischen den zahlen, 1. Variante
+        //std::cout << i << std::endl; //mit leerer zeile zwischen den zahlen, 2. Variante
+
+    }
+
+    return 0;
+}
