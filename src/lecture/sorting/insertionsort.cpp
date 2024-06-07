e4fbfde7e0cacceba33a2a31d725cdefa694c664
Laur0t0 <corvin2505@icloud.com>
2024-06-07 10:25:38 +0200

    07.06.24

--- src/lecture/sorting/insertionsort.cpp
+++ src/lecture/sorting/insertionsort.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-int main(){
-    std::vector<int> list {42,23,15,103,37,38};
+// int main(){
+//     std::vector<int> list {42,23,15,103,37,38};
 
-    insertionsort(list);
-    for(int element : list){
-        std::cout << element << " ";
-    }
-    std::cout << std::endl;
-}
+//     insertionsort(list);
+//     for(int element : list){
+//         std::cout << element << " ";
+//     }
+//     std::cout << std::endl;
+// }
 
-///insert erwartet eine Liste list und eine Position p
-///ertauscht das element an stelle p nach links, bis es entweder größer ist als sein linker Nachbar,
-///oder bis es ganz links ist
+// ///insert erwartet eine Liste list und eine Position p
+// ///ertauscht das element an stelle p nach links, bis es entweder größer ist als sein linker Nachbar,
+// ///oder bis es ganz links ist
 
-void insert(std::vector<int> list{42,23,15,103,37,38}){
+// void insert(std::vector<int> list{42,23,15,103,37,38}){
     
-}
+// }
 
-void insertionsort(){
+// void insertionsort(){
 
-}
+// }
